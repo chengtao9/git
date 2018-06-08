af4a28ad24f372d0c2e7f633c01ba14b1a259c6b
chengtao9 <exam20131@qq.com>
2018-06-08 16:52:30 +0800

    第四次提交

---
+++ src/main/java/com/baizhi/entity/TestUser.java
@@ -0,0 +1,10 @@
+package com.baizhi.entity;
+
+/**
+ * Created by lala on 2018/6/8.
+ */
+public class TestUser {
+    public static void main(String[] args) {
+        User user = new User();
+    }
+}
